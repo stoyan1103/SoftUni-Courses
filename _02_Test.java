f31ce8385dcae1db7f4759593f8e0a0008c79d99
stoyan1103 <zaikovstoyan@gmail.com>
2024-05-21 16:57:53 +0300

    New files added!

--- _02_Test.java
+++ _02_Test.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class _02_Test {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = Integer.parseInt(scanner.nextLine());
+
+        System.out.printf("Hi %s, you are %d years old!", name, age);
+    }
 }
